0075612af2943ddf6549876dec32c56ee4d253a6
aweigle827 <aweigle@ufl.edu>
2023-10-25 18:08:23 -0400

    Made changes

--- cop3502C-Lab6.py
+++ cop3502C-Lab6.py
@@ -11,6 +11,20 @@ def encode(password):
     for char in password:
         digit = (int(char) + 3) % 10
         encoded = encoded + str(digit)
+    return encoded
+def decode(encoded):
+    list = []
+    password = ''
+    for i in encoded:
+        if i < 3:
+            x = int(i) + 7
+            list.append(str(x))
+        else:
+            x = int(i) - 3
+            list.append(str(x))
+    for j in list:
+        password += j
+    return print(f'The encoded password is {encoded}, and the original password is {password}.')
 
 if __name__ == '__main__':
     password = input("Please enter a password: ")
